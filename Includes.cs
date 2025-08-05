da78fc86014c54946b6875d26c6af7fe77ebe1fb
Joaquin Muñiz <joaco.munix@gmail.com>
2025-08-04 21:19:28 -0300

    xx

---
+++ Includes.cs
@@ -0,0 +1,9 @@
+global using System;
+global using Godot;
+
+global using static Godot.GD;
+global using static GodotLib.Util.LogUtils;
+global using static GodotLib.Util.LoggingCategories;
+
+global using GodotArray = Godot.Collections.Array;
+global using GodotDictionary = Godot.Collections.Dictionary;
