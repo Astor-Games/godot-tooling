164d5c99a1fea0d091f3c9abe9b12999caecaf06
Joaquin Muñiz <joaco.munix@gmail.com>
2025-11-07 19:26:45 -0300

    Fix multiple issues and complete basic projectile logic.

--- Util/CollectionExtensions.cs
+++ Util/CollectionExtensions.cs
@@ -1,16 +1,20 @@
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using static System.Runtime.CompilerServices.MethodImplOptions;
 
 namespace GodotLib.Util;
 
 public static class CollectionExtensions
 {
-    public static void RemoveUnorderedAt<T>(this List<T> list, int index)
+    [MethodImpl(AggressiveInlining)]
+    public static void RemoveUnorderedAt<T>(this IList<T> list, int index)
     {
         var lastIndex = list.Count - 1;
         list[index] = list[lastIndex];
         list.RemoveAt(lastIndex);
     }
     
+    [MethodImpl(AggressiveInlining)]
     public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
     {
         if (dictionary.TryGetValue(key, out var result))
@@ -18,5 +22,22 @@ public static class CollectionExtensions
         
         dictionary.Add(key, defaultValue);
         return defaultValue;
+    }
+    
+    [MethodImpl(AggressiveInlining)]
+    public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> defaultValueFactory)
+    {
+        if (dictionary.TryGetValue(key, out var result))
+            return result;
+
+        var defaultValue = defaultValueFactory();
+        dictionary.Add(key, defaultValue);
+        return defaultValue;
+    }
+
+    [MethodImpl(AggressiveInlining)]
+    public static void Clear(this Array array)
+    {
+        Array.Clear(array);
     }
 }
