e0bc2f520aac8f267b19631603c87d736ec0d539
Joaquin Muñiz <joaco.munix@gmail.com>
2025-12-30 14:49:50 -0300

    More migration to ECS: spawners, regions, parts of character

---
+++ Geometry/Shape.cs
@@ -0,0 +1,100 @@
+using System.Diagnostics.Contracts;
+using MessagePack;
+
+namespace GodotLib;
+
+public enum Shape2DType : byte
+{
+    Circle,
+    Rect
+}
+
+[MessagePackObject]
+public struct Shape2D
+{
+    [Key(0)] public Shape2DType Type;
+
+    // Shared
+    [Key(1)] public Vector2 Center;
+
+    // Circle
+    [Key(2)] public float Radius;
+
+    // Rect
+    [Key(3)] public Vector2 HalfSize;
+  
+    public static Shape2D Circle(Vector2 center, float radius)
+    {
+        return new Shape2D
+        {
+            Type = Shape2DType.Circle,
+            Center = center,
+            Radius = radius
+        };
+    }
+
+    public static Shape2D Rect(Vector2 center, Vector2 size)
+    {
+        return new Shape2D
+        {
+            Type = Shape2DType.Rect,
+            Center = center,
+            HalfSize = size * 0.5f
+        };
+    }
+
+    
+    [Pure]
+    public Shape2D Shrink(float delta)
+    {
+        return Grow(-delta);
+    }
+
+    [Pure]
+    public Shape2D Grow(float delta)
+    {
+        var copy = this;
+        switch (Type)
+        {
+            case Shape2DType.Circle:
+                copy.Radius = Mathf.Max(0f, Radius + delta);
+                break;
+
+            case Shape2DType.Rect:
+                var grown = HalfSize + new Vector2(delta, delta);
+                copy.HalfSize = grown.Max(Vector2.Zero);
+                break;
+        }
+        return copy;
+    }
+    
+    [Pure]
+    public Vector2 Sample(RandomNumberGenerator rng)
+    {
+        switch (Type)
+        {
+            case Shape2DType.Circle:
+            {
+                // sqrt for uniform area distribution
+                var r = Radius * MathF.Sqrt(rng.Randf());
+                var a = rng.Randf() * MathF.Tau;
+
+                return Center + new Vector2(
+                    MathF.Cos(a) * r,
+                    MathF.Sin(a) * r
+                );
+            }
+
+            case Shape2DType.Rect:
+            {
+                return Center + new Vector2(
+                    (rng.Randf() * 2f - 1f) * HalfSize.X,
+                    (rng.Randf() * 2f - 1f) * HalfSize.Y
+                );
+            }
+
+            default:
+                return Center;
+        }
+    }
+}
---
+++ Geometry/ShapeUtils.cs
@@ -0,0 +1,9 @@
+namespace GodotLib;
+
+public static class ShapeUtils
+{
+    extension(in Shape2D shape)
+    {
+       
+    }
+}
