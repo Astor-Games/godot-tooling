b0a2299ac73e03c0a45dae21beebbed285e2ee9c
Joaquin Muñiz <joaco.munix@gmail.com>
2026-01-19 12:28:51 -0300

    Refactor logging system.
    Replaced `Logger.Log` calls with severity-specific methods. Enhanced the logger class with features like colored console output and structured variable logging.

--- Logging/Logger.cs
+++ Logging/Logger.cs
@@ -1,114 +1,185 @@
+using System.Diagnostics;
 using System.Text;
-
+using System.Threading;
+using Utf8Json;
 using Arg = System.Runtime.CompilerServices.CallerArgumentExpressionAttribute;
 
 // ReSharper disable once CheckNamespace
 namespace GodotLib.Logging;
 
-public class Logger
+public class Logger(string name)
 {
+    public enum LogOutput
+    {
+        GodotConsole,
+        DotNetConsole
+    }
+    
     public enum SeverityLevel
     {
-        Verbose = 1,
+        Debug = 1,
         Info = 2,
         Warning = 3,
         Error = 4
     }
-    
+
+    public static LogOutput Output;
     public SeverityLevel Level = SeverityLevel.Info;
-    public string Name { get; init; }
-    private readonly StringBuilder message = new(256);
-    private readonly StringBuilder prefix = new(64);
+    
+    
+    public string Name { get; init; } = name;
+    private readonly StringBuilder sb = new(256);
+    private static readonly Lock printLock = new();
 
-    public Logger(string name)
+    static Logger()
     {
-        Name = name;
-    }
-    
-    public void LogVerbose(string message)
-    {
-        if (Level < SeverityLevel.Verbose) return;
-        this.message.Append(message);
-        Print(SeverityLevel.Verbose);
-    }
-    
-    public void LogWarning(string message)
-    {
-        if (Level < SeverityLevel.Warning) return;
-        this.message.Append(message);
-        Print(SeverityLevel.Warning);
-    }
-    
-    public void LogError(string message)
-    {
-        if (Level < SeverityLevel.Error) return;
-        this.message.Append(message);
-        Print(SeverityLevel.Error);
-    }
-    
-    public void Log(string message, SeverityLevel severityLevel = SeverityLevel.Info)
-    {
-        if (Level < severityLevel) return;
-        this.message.Append(message);
-        Print(severityLevel);
+        Output = LogOutput.DotNetConsole;
     }
 
-    public void Log(object variable, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("variable")] string variableName = null)
+    [Conditional("DEBUG")]
+    public void Debug(string message)
     {
-       if (Level < severityLevel) return;
-        
-        message.Append(variableName).Append(": ").Append(variable);
-        Print(severityLevel);
+        if (Level > SeverityLevel.Debug) return;
+        sb.Append(message);
+        PrintLog(SeverityLevel.Debug);
+    }
+
+    [Conditional("DEBUG")]
+    public void Debug(string message, object variable, [Arg("variable")] string variableName = null)
+    {
+        if (Level > SeverityLevel.Debug) return;
+        sb.Append(message);
+        sb.Append(" | ");
+        sb.AppendVar(variableName, variable);
+        PrintLog(SeverityLevel.Debug);
     }
     
-    public void Log(object var1, object var2, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null)
+    public void Info(string message)
     {
-       if (Level < severityLevel) return;
-        
-        message.Append(name1).Append(": ").Append(var1).Append(", ");
-        message.Append(name2).Append(": ").Append(var2);
-        Print(severityLevel);
+        if (Level > SeverityLevel.Info) return;
+        sb.Append(message);
+        PrintLog(SeverityLevel.Info);
     }
     
-    public void Log(object var1, object var2, object var3, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null, [Arg("var3")] string name3 = null)
+    public void Info(string message, object variable, [Arg("variable")] string variableName = null)
     {
-       if (Level < severityLevel) return;
-        
-        message.Append(name1).Append(": ").Append(var1).Append(", ");
-        message.Append(name2).Append(": ").Append(var2).Append(", ");
-        message.Append(name3).Append(": ").Append(var3);
-        Print(severityLevel);
+        if (Level > SeverityLevel.Info) return;
+        sb.Append(message);
+        sb.Append(" | ");
+        sb.AppendVar(variableName, variable);
+        PrintLog(SeverityLevel.Info);
     }
     
-    public void Log(object var1, object var2, object var3, object var4, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null, [Arg("var3")] string name3 = null, [Arg("var4")] string name4 = null)
+    public void Warning(string message)
     {
-       if (Level < severityLevel) return;
-        
-        message.Append(name1).Append(": ").Append(var1).Append(", ");
-        message.Append(name2).Append(": ").Append(var2).Append(", ");
-        message.Append(name3).Append(": ").Append(var3).Append(", ");
-        message.Append(name4).Append(": ").Append(var4);
-        Print(severityLevel);
+        if (Level > SeverityLevel.Warning) return;
+        sb.Append(message);
+        PrintLog(SeverityLevel.Warning);
     }
     
-    private void Print(SeverityLevel severityLevel)
+    public void Warning(string message, object variable, [Arg("variable")] string variableName = null)
     {
-        if (severityLevel == SeverityLevel.Warning)
+        if (Level > SeverityLevel.Warning) return;
+        sb.Append(message);
+        sb.Append(" | ");
+        sb.AppendVar(variableName, variable);
+        PrintLog(SeverityLevel.Warning);
+    }
+    
+    public void Error(string message)
+    {
+        if (Level > SeverityLevel.Error) return;
+        sb.Append(message);
+        PrintLog(SeverityLevel.Error);
+    }
+    
+    public void Error(string message, object variable, [Arg("variable")] string variableName = null)
+    {
+        if (Level > SeverityLevel.Error) return;
+        sb.Append(message);
+        sb.Append(" | ");
+        sb.AppendVar(variableName, variable);
+        PrintLog(SeverityLevel.Error);
+    }
+    
+
+    public void PrintVar(object variable, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("variable")] string variableName = null)
+    {
+        if (Level > severityLevel) return;
+        sb.AppendVar(variableName, variable);
+        PrintLog(severityLevel);
+    }
+    
+    public void PrintVars(object var1, object var2, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null)
+    {
+        if (Level > severityLevel) return;
+        
+        sb.AppendVar(name1, var1).Append(", ");
+        sb.AppendVar(name2, var2);
+        PrintLog(severityLevel);
+    }
+    
+    public void PrintVars(object var1, object var2, object var3, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null, [Arg("var3")] string name3 = null)
+    {
+        if (Level > severityLevel) return;
+        
+        sb.AppendVar(name1, var1).Append(", ");
+        sb.AppendVar(name2, var2).Append(", ");
+        sb.AppendVar(name3, var3);
+        PrintLog(severityLevel);
+    }
+    
+    public void PrintVars(object var1, object var2, object var3, object var4, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null, [Arg("var3")] string name3 = null, [Arg("var4")] string name4 = null)
+    {
+        if (Level > severityLevel) return;
+        
+        sb.AppendVar(name1, var1).Append(", ");
+        sb.AppendVar(name2, var2).Append(", ");
+        sb.AppendVar(name3, var3).Append(", ");
+        sb.AppendVar(name4, var4);
+        PrintLog(severityLevel);
+    }
+    
+    private void PrintLog(SeverityLevel severityLevel)
+    {
+        
+        var formattedMessage = $"[{Name}] [{severityLevel}] {sb}";
+        
+        switch (Output)
         {
-            prefix.Append("[color=").Append(Colors.Gold).Append(']');
-            message.Append("[color=").Append(Colors.Gold).Append(']');
-        } else if (severityLevel == SeverityLevel.Error)
-        {
-            prefix.Append("[color=").Append(Colors.Crimson).Append(']');
-            message.Append("[color=").Append(Colors.Crimson).Append(']');
+            case LogOutput.GodotConsole:
+                Print(formattedMessage);
+                break;
+            
+            case LogOutput.DotNetConsole:
+                
+                var ansiColor = severityLevel switch
+                {
+                    SeverityLevel.Debug => ConsoleColor.Gray,
+                    SeverityLevel.Info => ConsoleColor.Black,
+                    SeverityLevel.Warning => ConsoleColor.Yellow,
+                    SeverityLevel.Error => ConsoleColor.Red,
+                    _ => ConsoleColor.White
+                };
+                
+                lock (printLock)
+                {
+                    Console.ForegroundColor = ansiColor;
+                    Console.WriteLine(formattedMessage);
+                    Console.ResetColor();
+                }
+                break;
         }
+        
+        sb.Clear();
+    }
+}
 
-        prefix.Append('[').Append(severityLevel).Append("] [").Append(Name).Append("] ");
-        
-        var finalString = prefix.Append(message).ToString();
-        PrintRich(finalString);
-        
-        prefix.Clear();
-        message.Clear();
+public static class StringBuilderExt
+{
+    public static StringBuilder AppendVar(this StringBuilder sb, string varName, object var)
+    {
+        var json = JsonSerializer.PrettyPrint(JsonSerializer.Serialize(var));
+        return sb.Append(varName).Append(": ").Append(json);
     }
 }
