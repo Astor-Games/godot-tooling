ad12ac6c0960aa291f1d88938d0ddba8903b0520
Joaquin Muñiz <joaco.munix@gmail.com>
2026-01-18 22:11:51 -0300

    Refactor logger to support severity levels and add source generator for [Flags] enums extension methods.

---
+++ Analyzers/src/SourceGenerators/FlagsEnumExtensionsGenerator.cs
@@ -0,0 +1,85 @@
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace GodotLib.Analyzers.SourceGenerators;
+
+[Generator]
+public class FlagsEnumExtensionsGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // Filter enums that have the [Flags] attribute
+        var flagsEnums = context.SyntaxProvider
+            .CreateSyntaxProvider(
+                predicate: static (node, _) => node is EnumDeclarationSyntax,
+                transform: static (ctx, _) => GetFlagsEnumOrNull(ctx))
+            .Where(static enumInfo => enumInfo is not null)
+            .Select(static (enumInfo, _) => enumInfo!);
+
+        // Generate extension classes for each flags enum
+        context.RegisterSourceOutput(flagsEnums, static (spc, enumInfo) =>
+        {
+            var source = GenerateExtensionClass(enumInfo.Value.Symbol, enumInfo.Value.Namespace);
+            spc.AddSource($"{enumInfo.Value.Symbol.Name}Ext.generated.cs", source);
+        });
+    }
+
+    private static (INamedTypeSymbol Symbol, string? Namespace)? GetFlagsEnumOrNull(GeneratorSyntaxContext context)
+    {
+        var enumDeclaration = (EnumDeclarationSyntax)context.Node;
+
+        if (context.SemanticModel.GetDeclaredSymbol(enumDeclaration) is not INamedTypeSymbol enumSymbol)
+            return null;
+
+        // Check if enum has [Flags] attribute
+        var hasFlagsAttribute = enumSymbol.GetAttributes().Any(attr =>
+            attr.AttributeClass?.Name is "FlagsAttribute" or "Flags");
+
+        if (!hasFlagsAttribute)
+            return null;
+
+        var namespaceName = enumSymbol.ContainingNamespace.IsGlobalNamespace
+            ? null
+            : enumSymbol.ContainingNamespace.ToDisplayString();
+
+        return (enumSymbol, namespaceName);
+    }
+
+    private static string GenerateExtensionClass(INamedTypeSymbol enumSymbol, string? namespaceName)
+    {
+        var enumName = enumSymbol.Name;
+
+        var sb = new StringBuilder();
+        sb.AppendLine("using System.Runtime.CompilerServices;");
+        sb.AppendLine();
+
+        if (!string.IsNullOrEmpty(namespaceName))
+        {
+            sb.AppendLine($"namespace {namespaceName};");
+            sb.AppendLine();
+        }
+
+        sb.AppendLine($"public static class {enumName}Ext");
+        sb.AppendLine("{");
+
+        // Generate Get method
+        sb.AppendLine("    [MethodImpl(MethodImplOptions.AggressiveInlining)]");
+        sb.AppendLine($"    public static bool Get(this {enumName} mask, {enumName} flag)");
+        sb.AppendLine("    {");
+        sb.AppendLine("        return (mask & flag) != 0;");
+        sb.AppendLine("    }");
+        sb.AppendLine();
+
+        // Generate Set method
+        sb.AppendLine("    [MethodImpl(MethodImplOptions.AggressiveInlining)]");
+        sb.AppendLine($"    public static void Set(this ref {enumName} mask, {enumName} flag, bool value)");
+        sb.AppendLine("    {");
+        sb.AppendLine("        mask = (value ? mask | flag : mask & ~flag);");
+        sb.AppendLine("    }");
+        sb.AppendLine("}");
+
+        return sb.ToString();
+    }
+}
--- Logging/Logger.cs
+++ Logging/Logger.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Text;
 
 using Arg = System.Runtime.CompilerServices.CallerArgumentExpressionAttribute;
@@ -8,64 +7,108 @@ namespace GodotLib.Logging;
 
 public class Logger
 {
-    public bool Enabled { get; set; } = true;
+    public enum SeverityLevel
+    {
+        Verbose = 1,
+        Info = 2,
+        Warning = 3,
+        Error = 4
+    }
+    
+    public SeverityLevel Level = SeverityLevel.Info;
     public string Name { get; init; }
-    private readonly StringBuilder sb = new(256);
-    private string prefix = "";
+    private readonly StringBuilder message = new(256);
+    private readonly StringBuilder prefix = new(64);
 
     public Logger(string name)
     {
         Name = name;
     }
     
-    public void Log(string message)
+    public void LogVerbose(string message)
     {
-        if (!Enabled) return;
-        sb.Append(message);
-        Print();
+        if (Level < SeverityLevel.Verbose) return;
+        this.message.Append(message);
+        Print(SeverityLevel.Verbose);
+    }
+    
+    public void LogWarning(string message)
+    {
+        if (Level < SeverityLevel.Warning) return;
+        this.message.Append(message);
+        Print(SeverityLevel.Warning);
+    }
+    
+    public void LogError(string message)
+    {
+        if (Level < SeverityLevel.Error) return;
+        this.message.Append(message);
+        Print(SeverityLevel.Error);
+    }
+    
+    public void Log(string message, SeverityLevel severityLevel = SeverityLevel.Info)
+    {
+        if (Level < severityLevel) return;
+        this.message.Append(message);
+        Print(severityLevel);
     }
 
-    public void Log(object variable, [Arg("variable")] string variableName = null)
+    public void Log(object variable, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("variable")] string variableName = null)
     {
-        if (!Enabled) return;
+       if (Level < severityLevel) return;
         
-        sb.Append(variableName).Append(": ").Append(variable);
-        Print();
+        message.Append(variableName).Append(": ").Append(variable);
+        Print(severityLevel);
     }
     
-    public void Log(object var1, object var2, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null)
+    public void Log(object var1, object var2, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null)
     {
-        if (!Enabled) return;
+       if (Level < severityLevel) return;
         
-        sb.Append(name1).Append(": ").Append(var1).Append(", ");
-        sb.Append(name2).Append(": ").Append(var2);
-        Print();
+        message.Append(name1).Append(": ").Append(var1).Append(", ");
+        message.Append(name2).Append(": ").Append(var2);
+        Print(severityLevel);
     }
     
-    public void Log(object var1, object var2, object var3, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null, [Arg("var3")] string name3 = null)
+    public void Log(object var1, object var2, object var3, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null, [Arg("var3")] string name3 = null)
     {
-        if (!Enabled) return;
+       if (Level < severityLevel) return;
         
-        sb.Append(name1).Append(": ").Append(var1).Append(", ");
-        sb.Append(name2).Append(": ").Append(var2).Append(", ");
-        sb.Append(name3).Append(": ").Append(var3);
-        Print();
+        message.Append(name1).Append(": ").Append(var1).Append(", ");
+        message.Append(name2).Append(": ").Append(var2).Append(", ");
+        message.Append(name3).Append(": ").Append(var3);
+        Print(severityLevel);
     }
     
-    public void Log(object var1, object var2, object var3, object var4, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null, [Arg("var3")] string name3 = null, [Arg("var4")] string name4 = null)
+    public void Log(object var1, object var2, object var3, object var4, SeverityLevel severityLevel = SeverityLevel.Info, [Arg("var1")] string name1 = null, [Arg("var2")] string name2 = null, [Arg("var3")] string name3 = null, [Arg("var4")] string name4 = null)
     {
-        if (!Enabled) return;
+       if (Level < severityLevel) return;
         
-        sb.Append(name1).Append(": ").Append(var1).Append(", ");
-        sb.Append(name2).Append(": ").Append(var2).Append(", ");
-        sb.Append(name3).Append(": ").Append(var3).Append(", ");
-        sb.Append(name4).Append(": ").Append(var4);
-        Print();
+        message.Append(name1).Append(": ").Append(var1).Append(", ");
+        message.Append(name2).Append(": ").Append(var2).Append(", ");
+        message.Append(name3).Append(": ").Append(var3).Append(", ");
+        message.Append(name4).Append(": ").Append(var4);
+        Print(severityLevel);
     }
     
-    private void Print()
+    private void Print(SeverityLevel severityLevel)
     {
-        GD.Print($"[{Name}] {sb}");
-        sb.Clear();
+        if (severityLevel == SeverityLevel.Warning)
+        {
+            prefix.Append("[color=").Append(Colors.Gold).Append(']');
+            message.Append("[color=").Append(Colors.Gold).Append(']');
+        } else if (severityLevel == SeverityLevel.Error)
+        {
+            prefix.Append("[color=").Append(Colors.Crimson).Append(']');
+            message.Append("[color=").Append(Colors.Crimson).Append(']');
+        }
+
+        prefix.Append('[').Append(severityLevel).Append("] [").Append(Name).Append("] ");
+        
+        var finalString = prefix.Append(message).ToString();
+        PrintRich(finalString);
+        
+        prefix.Clear();
+        message.Clear();
     }
 }
